d5241d397253e84ec97e33ea7986fa866a22eacf
Rune Ibsen <rui@copenhagensoftware.com>
2015-03-29 16:43:33 +0200

    Implemented partial Ast substitute method

---
+++ flap.Tests/AstAnalyserTests.fs
@@ -0,0 +1,38 @@
+﻿module AstAnalyserTests
+  open Ast
+  open AstAnalyser
+  open Xunit
+  open Xunit.Extensions
+  open Ploeh.AutoFixture.Xunit
+
+  [<Theory>]
+  [<AutoData>]
+  let SubstitutingIntoConstDoesNothing (p : string) (expr : Expr) i =
+    let cst = CstI(i)
+
+    let actual = substitute p expr cst
+
+    Assert.Equal(cst, actual)
+
+  [<Theory>]
+  [<AutoData>]
+  let SubstitutingIntoVarWhenItIsNotSubstitutionVarDoesNothing 
+        (p : string) (var : string) (expr : Expr) =
+    let varExpr = Var(var)
+
+    let actual = substitute p expr varExpr
+
+    Assert.NotEqual(p, var);
+    Assert.Equal(varExpr, actual)
+
+  [<Theory>]
+  [<AutoData>]
+  let SubstitutingIntoVarWhenItIsSubstitutionVarResultsInSubstitedExpression
+        (p : string) (expr : Expr) =
+    let varExpr = Var(p)
+
+    let actual = substitute p expr varExpr
+
+    Assert.Equal(expr, actual)
+
+
---
+++ flap/AstAnalyser.fs
@@ -0,0 +1,8 @@
+﻿module AstAnalyser
+  open Ast
+
+  let substitute p expr exprIn = 
+    match exprIn with
+    | CstI(_) as cst -> cst
+    | Var(v) when v = p -> expr
+    | Var(v) as var -> var
