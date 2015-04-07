5f12052a3328f17bb2736e940a05ee936fb8d2f5
Rune Ibsen <rui@copenhagensoftware.com>
2015-04-07 18:21:48 +0200

    Parses positive integer

---
+++ flap.Tests/ParserTests.fs
@@ -0,0 +1,15 @@
+﻿module ParserTests
+  open Ast
+  open Parser
+  open Xunit
+  open Xunit.Extensions
+  open Ploeh.AutoFixture.Xunit
+
+  [<Theory>]
+  [<AutoData>]
+  let parsesInteger (i : int) =
+    let str = i.ToString()
+
+    let actual = parse str
+
+    Assert.Equal(CstI(i), actual)
---
+++ flap/Parser.fs
@@ -0,0 +1,13 @@
+﻿module Parser
+  open Ast
+  open FParsec
+
+  let parse s =
+    
+    let parseInteger = numberLiteral NumberLiteralOptions.DefaultInteger "integer"
+
+    let result = runParserOnString parseInteger null "input" s
+
+    match result with
+    | ParserResult.Success(res, ustate, position) -> CstI(int res.String)
+    | ParserResult.Failure(str, parseError, ustate) -> failwith (parseError.ToString())
